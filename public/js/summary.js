8f93f7cfd0616c64634c026c1ceef147caa50db7
mstuazon <marie.sheridan.tuazon@gmail.com>
2016-02-10 19:20:26 +0800

    Show persons

--- public/js/summary.js
+++ public/js/summary.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     var divId = "#app-summary";
-    computeTotalUnits(items);
     showItemSummary(divId, items);
+    showPersonsSummary(divId, persons, items);
 });
 
 function showItemSummary(divId, itemsObject)
@@ -9,21 +9,94 @@ function showItemSummary(divId, itemsObject)
     var appendValue = "<div id='summary-block'>";
     appendValue += divForHeader('Overview');
     appendValue += divForLine();
-    var counter = 1;
-    for (iter1 in itemsObject)
+    for (iter in itemsObject)
     {
         appendValue += "<div class='app-item-block'>";
-        appendValue += divForItem(iter1);
-        appendValue += divForPrice(itemsObject[iter1]['itemPrice']);
+        appendValue += divForItem(iter);
+        appendValue += divForPrice(itemsObject[iter]['itemPrice'].toFixed(2));
         appendValue += "</div>";
-        counter++;
     }
     appendValue += divForLine();
     appendValue += divForItem('Total Price');
-    appendValue += divForPrice(computeTotalPrice(itemsObject));
+    appendValue += divForPrice(computeTotalPrice(itemsObject).toFixed(2));
     appendValue += "</div>";
 
     $(divId).append(appendValue);
+}
+
+function showPersonsSummary(divId, personsArray, itemsObject)
+{
+    computeTotalUnits(items);
+    for (iter in personsArray)
+    {
+        var name = personsArray[iter];
+        console.log("showPersonsSummary: " + personsArray[iter]);
+        if (isPersonPaying(name, itemsObject))
+        {
+            console.log("showPersonsSummary 2: " + name);
+            showPerson(divId, name, itemsObject);
+        }
+    }
+}
+
+function showPerson(divId, personName, itemsObject)
+{
+    var payTotal = 0;
+    var appendValue = divForSpacer();
+    appendValue += "<div class='person-summary'>";
+    appendValue += divForHeader(personName);
+    appendValue += divForLine();
+    for (iter in itemsObject)
+    {
+        if(isPersonPayingForItem(personName, itemsObject[iter]))
+        {
+            var quantity = itemsObject[iter]['buyers'][personName];
+            var totalUnits = itemsObject[iter]['totalUnits'];
+            var unitPrice = itemsObject[iter]['itemPrice'] / totalUnits;
+            var payPrice = unitPrice * quantity;
+            payTotal += payPrice;
+
+            appendValue += "<div class='app-item-block'>";
+            appendValue += divForItem(iter);
+            appendValue += divForQuantity(quantity);
+            appendValue += divForUnitPrice(unitPrice.toFixed(2));
+            appendValue += divForPrice(payPrice.toFixed(2));
+            appendValue += "</div>";
+        }
+    }
+    appendValue += divForLine();
+    appendValue += divForItem('Total');
+    appendValue += divForPlaceholder();
+    appendValue += divForPrice(payTotal.toFixed(2));
+    appendValue += divForSpacer();
+    appendValue += "</div>";
+    $(divId).append(appendValue);
+}
+
+function isPersonPaying(personName, itemsObject)
+{
+    var personIsPaying = false;
+    for (iter in itemsObject)
+    {
+        if (itemsObject[iter]['buyers'].hasOwnProperty(personName))
+        {
+            personIsPaying = true;
+        }
+    }
+    console.log(personName + ' is paying = ' + personIsPaying);
+    return personIsPaying;
+}
+
+function isPersonPayingForItem(personName, itemArray)
+{
+    if (itemArray['buyers'].hasOwnProperty(personName))
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
 
 function computeTotalUnits(itemsObject)
@@ -57,13 +130,25 @@ function divForHeader(title)
 
 function divForItem(input)
 {
-    return "<div class='app-item-name'>"
+    return "<div class='summary-item-name'>"
            + input + "</div>";
 }
 
 function divForPrice(input)
 {
-    return "<div class='app-item-price'>"
+    return "<div class='summary-item-price'>"
+           + input + "</div>";
+}
+
+function divForQuantity(input)
+{
+    return "<div class='summary-qty'>"
+           + input + "</div>";
+}
+
+function divForUnitPrice(input)
+{
+    return "<div class='summary-unit-price'>@"
            + input + "</div>";
 }
 
@@ -72,4 +157,14 @@ function divForLine()
     return "<div class='app-line-space'></div>"
            + "<div class='app-line'></div>"
            + "<div class='app-line-space'></div>";
+}
+
+function divForPlaceholder()
+{
+    return "<div class='summary-placeholder'>&nbsp</div>";
+}
+
+function divForSpacer()
+{
+    return "<div class='app-spacer'></div>";
 }
